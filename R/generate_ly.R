96c6dd36b2d4c580b72bf77fc64c47b8f00ecd3a
andrealanz <lanza@oregonstate.edu>
2018-11-30 23:52:13 +0000

    combined two functions

--- R/generate_ly.R
+++ R/generate_ly.R
@@ -1,16 +1,11 @@
 
-generate_ly <- function(sample, file_name = "my_song.ly") {
-  n <- length(sample)
-  Wobj <- sample[[1]]
-  for(i in 2:n){
-    Wobj <- bind(Wobj, sample[[i]])
-  }
-  
+generate_ly_rhythm <- function(sample, bars, file_name = "my_song.ly") {
+  Wobj <- reduce(sample, bind)
   WspecObject <- periodogram(Wobj, normalize = TRUE, width = 1024)
   ff <- FF(WspecObject)
   notes <- smoother(noteFromFF(ff))
   melodyplot(WspecObject, notes)
-  qlily <- quantMerge(notes = notes, bars = n/4, barsize = 8, minlength = 8)
+  qlily <- quantMerge(notes = notes, bars = bars, barsize = 8, minlength = 8)
   
   lilyinput(qlily, file = file_name)
 }
